824e7a8bcb62f19a3d926f3e9fbdc59c9ecccae8
Tyler Caraza-Harter <tharter@wisc.edu>
2023-04-05 16:20:42 +0000

    lec 25 demo

--- lec/25-cassandra/cassandra.sh
+++ lec/25-cassandra/cassandra.sh
@@ -3,7 +3,7 @@ echo "-Xmx128M" >> /apache-cassandra-4.0.7/conf/jvm-server.options
 
 sed -i "s/^listen_address:.*/listen_address: "`hostname`"/" /apache-cassandra-4.0.7/conf/cassandra.yaml
 sed -i "s/^rpc_address:.*/rpc_address: "`hostname`"/" /apache-cassandra-4.0.7/conf/cassandra.yaml
-sed -i "s/- seeds:.*/- seeds: 24-cassandra-db-1,24-cassandra-db-2,24-cassandra-db-3/" /apache-cassandra-4.0.7/conf/cassandra.yaml
+sed -i "s/- seeds:.*/- seeds: 25-cassandra-db-1,25-cassandra-db-2,25-cassandra-db-3/" /apache-cassandra-4.0.7/conf/cassandra.yaml
 
 /apache-cassandra-4.0.7/bin/cassandra -R
 
